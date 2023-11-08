68f0164a94d16ac3cd0bacca659d1b065634d38b
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2023-11-08 19:45:21 +0600

    added banner to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Footer from "../../Shared/Footer";
 import NavBar from "../../Shared/NavBar";
+import Banner from "./Banner";
 import TabJobs from "./TabJobs";
 
 
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Banner></Banner>
            <div className="max-w-6xl mx-auto md:p-0 p-4 ">
            <TabJobs></TabJobs>
            </div>
